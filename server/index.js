cb09dfb8a230e05b1ec596c9c73a21f63b615889
Rio0628 <mariomdomenech@gmail.com>
2021-12-07 11:45:09 -0500

    Created Main Server File
    - Created the server side directory and the main index file for the server side.
    - Added the main database file that will connect the backend to the database.
    - Installed initial necessary packets for the application to run.

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+const app = express();
+const apiPort = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection erro:'))
+
+app.get('/', (req, res) => { res.send('Hello World!') });
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
