1d0db1d52775d2e6ed92c1b7aed34463d3224ddb
Rio0628 <mariomdomenech@gmail.com>
2021-12-09 12:09:41 -0500

    Applied Breakpoint Styling
    - Applied the breakpoint styling for when the viewport has a minimum width of 1300px and 1500px.
    - Removed the componentsExport file due to the fact that it is redundant.

--- src/components/componentsExport.js
+++
@@ -1,6 +0,0 @@
-import CurrentTaskView from './CurrentTasksView';
-import SavedTasksView from './SavedTasksView';
-import IndTodoTask from './IndTodoTask';
-import IndSavedTask from './IndSavedTask';
-
-export { CurrentTaskView, SavedTasksView, IndTodoTask, IndSavedTask}
