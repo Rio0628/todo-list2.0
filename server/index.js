db85d3faf9252e23d0974f11a4686a714616157e
Rio0628 <mariomdomenech@gmail.com>
2022-01-10 12:21:11 -0500

    Added Heroku Functionality
    - Added necessary dependencies like heroku cli to allow the service to run the application.
    - Added functionality to allow the service to run the application when it is in production.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const db = require('./db');
 const taskRouter = require('./routes/task-router');
 const app = express();
-const apiPort = 3000;
+const apiPort = process.env.apiPort || 3000; // New Line
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -13,5 +13,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => { res.send('Hello World!') });
 app.use('/api', taskRouter);
+
+// if (process.env.NODE_ENV === 'production') { // New Line
+//     app.use(express.static('./build'))
+// }
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
