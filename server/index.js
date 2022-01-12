083b4ed0a37b1e074e707d0586a64e09bea35021
Rio0628 <mariomdomenech@gmail.com>
2022-01-11 21:04:51 -0500

    First Commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./db');
 const taskRouter = require('./routes/task-router');
 const app = express();
-const apiPort = process.env.apiPort || 3000; // New Line
+const apiPort = process.env.PORT || 3000; // New Line
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -16,7 +16,10 @@ app.get('/', (req, res) => { res.send('Hello World!') });
 app.use('/api', taskRouter);
 
 if (process.env.NODE_ENV === 'production') { // New Line
-    app.use(express.static('../build'))
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    });
 }
 
 console.log(process.env.DB_PASSWORD)
