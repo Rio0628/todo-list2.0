8a8e7d1a367dff017e3bbc50481431955574b6ba
Rio0628 <mariomdomenech@gmail.com>
2022-01-14 15:38:45 -0500

    Added new rest API baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `https://todo-list-mario-domenech.herokuapp.com/api`, });
+const api = axios.create({ baseURL: `https://todo-list-mario-domenech.herokuapp.com`, });
 
 /* http://localhost:3000/api => API baseURL for dev  */
 
-export const insertTask = payload => api.post(`/task`, payload);
-export const getAllTasks = () => api.get(`/tasks`);
-export const updateTaskById = (id, payload) => api.put(`/task/${id}`, payload);
-export const deleteTaskById = id => api.delete(`/task/${id}`);
+export const insertTask = payload => api.post(`/api/task`, payload);
+export const getAllTasks = () => api.get(`/api/tasks`);
+export const updateTaskById = (id, payload) => api.put(`/api/task/${id}`, payload);
+export const deleteTaskById = id => api.delete(`/api/task/${id}`);
 
 const apis = { insertTask, getAllTasks, updateTaskById, deleteTaskById };
 export default apis;
