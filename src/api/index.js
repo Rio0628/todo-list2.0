fae2f461e5897534fdafc0584db095283f04e304
Rio0628 <mariomdomenech@gmail.com>
2022-01-15 22:59:15 -0500

    Changed restAPI Methods

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `/api`, });
+// const api = axios.create({ baseURL: `/api`, });
 
-export const insertTask = payload => api.post(`/task`, payload);
-export const getAllTasks = () => api.get(`/tasks`);
-export const updateTaskById = (id, payload) => api.put(`/task/${id}`, payload);
-export const deleteTaskById = id => api.delete(`/task/${id}`);
+export const insertTask = payload => axios.post(`/api/task`, payload);
+export const getAllTasks = () => axios.get(`/api/tasks`);
+export const updateTaskById = (id, payload) => axios.put(`/api/task/${id}`, payload);
+export const deleteTaskById = id => axios.delete(`/api/task/${id}`);
 
 const apis = { insertTask, getAllTasks, updateTaskById, deleteTaskById };
 export default apis;
