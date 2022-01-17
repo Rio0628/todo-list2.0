a22acfdfad792977ebbb7a0b1169784e73bd16e5
Rio0628 <mariomdomenech@gmail.com>
2022-01-16 23:27:46 -0500

    Altered router

--- server/routes/task-router.js
+++ server/routes/task-router.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.post('/task', TaskCtrl.createTask);
 router.put('/task/:id', TaskCtrl.updateTask);
 router.delete('/task/:id', TaskCtrl.deleteTask);
-router.get('/tasks', TaskCtrl.getTasks);
+router.get('/task', TaskCtrl.getTasks);
 
 module.exports = router;
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const api = axios.create({ baseURL: `/api`, });
 
 export const insertTask = payload => axios.post(`/api/task`, payload);
-export const getAllTasks = () => axios.get(`/api/tasks`);
+export const getAllTasks = () => axios.get(`/api/task`);
 export const updateTaskById = (id, payload) => axios.put(`/api/task/${id}`, payload);
 export const deleteTaskById = id => axios.delete(`/api/task/${id}`);
 
