5dfc9f5fd8a46a5b2daca7f0c5273e191c7abc6f
Rio0628 <mariomdomenech@gmail.com>
2022-01-17 11:40:15 -0500

    Fixed production conditional

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'PROD') {
+if (process.env.NODE_ENV === 'production') { 
     app.use(express.static(buildPath));
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(buildPath, 'index.html'));
