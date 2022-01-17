3876626ff5eb585c4ac4f78e47359fec7973b455
Rio0628 <mariomdomenech@gmail.com>
2022-01-16 21:00:45 -0500

    Added MONGODB_URI

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.REACT_APP_DB_USERNAME}:${process.env.REACT_APP_DB_PASSWORD}@newcluster.a2laf.mongodb.net/Todo-List2?retryWrites=true&w=majority`, { useNewUrlParser: true }).catch( e => console.error('Connection error', e.message) );
+mongoose.connect( process.env.MONGODB_URI || `mongodb+srv://${process.env.REACT_APP_DB_USERNAME}:${process.env.REACT_APP_DB_PASSWORD}@newcluster.a2laf.mongodb.net/Todo-List2?retryWrites=true&w=majority`, { useNewUrlParser: true }).catch( e => console.error('Connection error', e.message) );
 
 const db = mongoose.connection;
 module.exports = db;
